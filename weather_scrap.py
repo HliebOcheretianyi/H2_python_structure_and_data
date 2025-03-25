6c36ca2f13707eaf9b71e04466437f1d998ad39d
NoroiMusha37 <vo.melnyk37@gmail.com>
2025-03-25 14:38:30 +0200

    Update weather_scrap.py

--- weather_scrap.py
+++ weather_scrap.py
@@ -1,15 +1,15 @@
 import os
-
 from dotenv import load_dotenv
-
 import requests
+from datetime import datetime, timedelta
 
 load_dotenv()
 RSA_KEY = os.getenv("RSA_KEY", "")
 
 
-def generate_forecast(location: str, date):
-    url_base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+def generate_forecast(location: str):
+    url_base = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline"
+
     elements = [
         "datetime", "name", "resolvedAddress", "latitude", "longitude",
         "temp", "feelslike", "dew", "humidity", "precip", "precipprob", "preciptype",
@@ -18,19 +18,36 @@ def generate_forecast(location: str, date):
         "sunrise", "sunset", "moonphase", "conditions"
     ]
 
+    start_time = datetime.now()
+    current_hour = start_time.replace(minute=0, second=0, microsecond=0)
+
+    end_time = start_time + timedelta(hours=24)
+
     params = {
         "unitGroup": "metric",
         "elements": ",".join(elements),
         "include": "hours",
         "key": RSA_KEY,
-        "contentType": "json"
+        "contentType": "json",
+        "tz": "Europe/Kiev"
     }
 
-    url = f"{url_base_url}/{location}/{date}"
+    url = f"{url_base}/{location}/{start_time.strftime('%Y-%m-%d')}/{end_time.strftime('%Y-%m-%d')}"
 
-    response = requests.get(url, params=params, headers={"X-Api-Key": RSA_KEY})
+    response = requests.get(url, params=params)
 
     if response.status_code == requests.codes.ok:
-        return response.json()
+        data = response.json()
+
+        hours = []
+        for day in data.get("days", []):
+            for hour in day.get("hours", []):
+                hour_time = datetime.strptime(hour["datetime"], "%H:%M:%S").time()
+                full_hour_time = datetime.combine(datetime.strptime(day["datetime"], "%Y-%m-%d"), hour_time)
+
+                if full_hour_time >= current_hour:
+                    hours.append(hour)
+
+        return hours[:24]
     else:
         raise Exception(f"ERROR: {response.text}: {response.status_code}")
